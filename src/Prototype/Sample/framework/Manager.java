1dd28bef73fdd936780f430ef6f48f255b5b1546
f <hukuti145@gmail.com>
2021-08-21 23:36:08 +0900

    Prototype pattern

---
+++ src/Prototype/Sample/framework/Manager.java
@@ -0,0 +1,16 @@
+package Prototype.Sample.framework;
+
+import java.util.*;
+
+public class Manager {
+    private HashMap<String, Product> showcase = new HashMap<>();
+
+    public void register(String name, Product proto) {
+        showcase.put(name, proto);
+    }
+
+    public Product create(String protoname) {
+        Product p = showcase.get(protoname);
+        return p.createClone();
+    }
+}
---
+++ src/Prototype/Sample/framework/Product.java
@@ -0,0 +1,7 @@
+package Prototype.Sample.framework;
+
+public interface Product extends Cloneable {
+    void use(String s);
+
+    Product createClone();
+}
