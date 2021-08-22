e201d24ab532d717cf64195e207c6b92350b095c
f <hukuti145@gmail.com>
2021-08-22 23:35:20 +0900

    Abstract Factory pattern

---
+++ src/AbstractFactory/Sample/listfactory/ListFactory.java
@@ -0,0 +1,20 @@
+package AbstractFactory.Sample.listfactory;
+
+import AbstractFactory.Sample.factory.*;
+
+public class ListFactory extends Factory {
+    @Override
+    public Link createLink(String caption, String url) {
+        return new ListLink(caption, url);
+    }
+
+    @Override
+    public Tray createTray(String caption) {
+        return new ListTray(caption);
+    }
+
+    @Override
+    public Page createPage(String title, String author) {
+        return new ListPage(title, author);
+    }
+}
