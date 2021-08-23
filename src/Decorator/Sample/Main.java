c8183a75505c33f7ec73932153d0a618da1bec1e
f <hukuti145@gmail.com>
2021-08-24 08:10:50 +0900

    Decorator pattern

---
+++ src/Decorator/Sample/Main.java
@@ -0,0 +1,26 @@
+package Decorator.Sample;
+
+public class Main {
+    public static void main(String[] args) {
+        Display b1 = new StringDisplay("Hello, world.");
+        b1.show();
+        Display b2 = new SideBorder(b1, '#');
+        b2.show();
+        Display b3 = new FullBorder(b2);
+        b3.show();
+        System.out.println("");
+        Display b4 =
+            new SideBorder(
+                new FullBorder(
+                    new FullBorder(
+                        new SideBorder(
+                            new FullBorder(
+                                new StringDisplay("こんにちは。")
+                            ), '*'
+                        )
+                    )
+                ), '/'
+            );
+        b4.show();
+    }
+}
